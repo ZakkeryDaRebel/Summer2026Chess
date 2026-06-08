787858614c51c06562be890c01b7606084262739
Tristan Weech <twminebro@gmail.com>
2026-06-08 15:25:36 -0600

    add(PawnCalculator) start

---
+++ shared/src/main/java/chess/calculator/PawnCalculator.java
@@ -0,0 +1,16 @@
+package chess.calculator;
+
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPosition;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PawnCalculator implements MoveCalculator {
+
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition pos) {
+        Collection<ChessMove> pieceMoves = new ArrayList<>();
+
+        return pieceMoves;
+    }
+}
