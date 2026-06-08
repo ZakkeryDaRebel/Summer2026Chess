c1e439b0e6fc1836e51c0b8450d7d95c64642a0c
Tristan Weech <twminebro@gmail.com>
2026-06-08 14:47:01 -0600

    add(KingCalculator)

---
+++ shared/src/main/java/chess/calculator/KingCalculator.java
@@ -0,0 +1,14 @@
+package chess.calculator;
+
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPosition;
+import java.util.Collection;
+
+public class KingCalculator implements MoveCalculator {
+
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition pos) {
+        int[][] directions = {{1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}};
+        return spotVerifier(board, pos, directions);
+    }
+}
