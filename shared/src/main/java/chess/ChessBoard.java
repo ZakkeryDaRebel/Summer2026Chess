7e3a4df06fc1fb7ebca6a0ca01fa278c115c9a55
Tristan Weech <twminebro@gmail.com>
2026-06-08 13:51:26 -0600

    add(ChessBoard) equals and hashcode

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -62,5 +65,19 @@ public class ChessBoard {
             board[0][i] = new ChessPiece(white, pieces[i]);
             board[7][i] = new ChessPiece(black, pieces[i]);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(board);
     }
 }
