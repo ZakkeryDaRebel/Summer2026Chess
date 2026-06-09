8fc1132f60361ac69df71f23c8f6a6cc13403e0d
Tristan Weech <twminebro@gmail.com>
2026-06-09 10:32:24 -0600

    add(ChessGame) isInCheckmate and isInStalemate functionality

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -128,7 +128,7 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        return isInCheck(teamColor) && !anyValidMoves(teamColor);
     }
 
     /**
@@ -139,7 +139,7 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        return !isInCheck(teamColor) && !anyValidMoves(teamColor);
     }
 
     /**
