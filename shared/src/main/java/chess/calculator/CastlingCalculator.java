dedbb84f5aebc8cf1548fcb6ba69da1fe672760f
Tristan Weech <twminebro@gmail.com>
2026-06-10 11:12:49 -0600

    add(CastlingCalculator) for castling functionality (should have separated this into separate commits, but copied a good amount of it from my old code)

---
+++ shared/src/main/java/chess/calculator/CastlingCalculator.java
@@ -0,0 +1,110 @@
+package chess.calculator;
+
+import chess.*;
+
+import java.util.Collection;
+
+public class CastlingCalculator {
+    public void canCastle(boolean[][] castlingPermissions, ChessBoard board, ChessPosition startPos,
+                          Collection<ChessMove> validMoves) {
+        ChessGame.TeamColor color = board.getPiece(startPos).getTeamColor();
+        int side = color == ChessGame.TeamColor.WHITE ? 0 : 1;
+
+        if (!castlingPermissions[side][1]) {
+            return;
+        }
+        if (castlingPermissions[side][0] && checkCastleQueen(board, startPos, color)) {
+            validMoves.add(new ChessMove(startPos, new ChessPosition(startPos.getRow(), 3), null));
+        }
+        if (castlingPermissions[side][2] && checkCastleKing(board, startPos, color)) {
+            validMoves.add(new ChessMove(startPos, new ChessPosition(startPos.getRow(), 7), null));
+        }
+    }
+
+    public ChessMove isCastleMove(ChessMove move, boolean[][] castlingPermissions) {
+        ChessPosition startPos = move.getStartPosition();
+        ChessPosition endPos = move.getEndPosition();
+        if (Math.abs(startPos.getColumn() - endPos.getColumn()) != 2 ||
+            startPos.getRow() != endPos.getRow()) {
+            return null;
+        }
+
+        int side = startPos.getRow() == 0 ? 0 : 1;
+        castlingPermissions[side][1] = false;
+
+        boolean castleQueen = startPos.getColumn() > endPos.getColumn();
+        int startCol = castleQueen ? 1 : 8;
+        int endCol = castleQueen ? 4 : 6;
+        return new ChessMove(new ChessPosition(startPos.getRow(), startCol),
+                new ChessPosition(startPos.getRow(), endCol), null);
+
+    }
+
+    public void loadBoard(ChessBoard board, boolean[][] castlingPermissions) {
+        ChessPiece whiteRook = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+        ChessPiece blackRook = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK);
+        ChessPiece whiteKing = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
+        ChessPiece blackKing = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING);
+
+        ChessPiece pieceAOne = board.getPiece(new ChessPosition(1, 1));
+        ChessPiece pieceEOne = board.getPiece(new ChessPosition(1, 5));
+        ChessPiece pieceHOne = board.getPiece(new ChessPosition(1, 8));
+        ChessPiece pieceAEight = board.getPiece(new ChessPosition(8, 1));
+        ChessPiece pieceEEight = board.getPiece(new ChessPosition(8, 5));
+        ChessPiece pieceHEight = board.getPiece(new ChessPosition(8, 8));
+
+        castlingPermissions[0][0] = (pieceAOne != null && pieceAOne.equals(whiteRook));
+        castlingPermissions[0][1] = (pieceEOne != null && pieceEOne.equals(whiteKing));
+        castlingPermissions[0][2] = (pieceHOne != null && pieceHOne.equals(whiteRook));
+        castlingPermissions[1][0] = (pieceAEight != null && pieceAEight.equals(blackRook));
+        castlingPermissions[1][1] = (pieceEEight != null && pieceEEight.equals(blackKing));
+        castlingPermissions[1][2] = (pieceHEight != null && pieceHEight.equals(blackRook));
+    }
+
+    public void updateCastlingPermissions(ChessMove move, ChessBoard board, boolean[][] castlingPermissions) {
+        ChessPiece checkPiece = board.getPiece(move.getEndPosition());
+        if (checkPiece.getPieceType() == ChessPiece.PieceType.KING) {
+            int pieceColor = checkPiece.getTeamColor() == ChessGame.TeamColor.WHITE ? 0 : 1;
+            castlingPermissions[pieceColor][1] = false;
+        }
+        captureCorner(move, castlingPermissions);
+    }
+
+    public boolean checkCastleQueen(ChessBoard board, ChessPosition startPos, ChessGame.TeamColor color) {
+        ChessPosition[] queenSideSpots = {
+                new ChessPosition(startPos.getRow(), 2),
+                new ChessPosition(startPos.getRow(), 3),
+                new ChessPosition(startPos.getRow(), 4)
+        };
+
+        return checkSpots(board, color, queenSideSpots);
+    }
+
+    public boolean checkCastleKing(ChessBoard board, ChessPosition startPos, ChessGame.TeamColor color) {
+        ChessPosition[] kingSideSpots = {
+                new ChessPosition(startPos.getRow(), 6),
+                new ChessPosition(startPos.getRow(), 7)
+        };
+
+        return checkSpots(board, color, kingSideSpots);
+    }
+
+    private boolean checkSpots(ChessBoard board, ChessGame.TeamColor color, ChessPosition[] spots) {
+        for (ChessPosition pos : spots) {
+            if (board.getPiece(pos) != null || AttackKingCalculator.canAttackKing(board, color, pos)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void captureCorner(ChessMove move, boolean[][] castlingPermissions) {
+        int[][] corners = {{1, 1}, {1, 8}, {8, 8}, {8, 1}};
+        int[][] castlingLocation = {{0, 0}, {0, 2}, {1, 2}, {1, 0}};
+        for (int i = 0; i < corners.length; i++) {
+            if (move.getEndPosition().equals(new ChessPosition(corners[i][0], corners[i][1]))) {
+                castlingPermissions[castlingLocation[i][0]][castlingLocation[i][1]] = false;
+            }
+        }
+    }
+}
