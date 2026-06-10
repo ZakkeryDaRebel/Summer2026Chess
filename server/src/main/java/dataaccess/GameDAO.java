2cf34ea508a92ce4e877bdf48732099da9b3fdd1
Tristan Weech <twminebro@gmail.com>
2026-06-10 13:59:45 -0600

    add(GameDAO) crud methods to the interface

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,9 @@
 package dataaccess;
 
+import model.GameData;
+
 public interface GameDAO {
+    void createGame(GameData game) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
 }
