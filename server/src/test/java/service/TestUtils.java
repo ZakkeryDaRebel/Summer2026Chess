8e2c9ea07af58e752372399350339581d263e90e
Tristan Weech <twminebro@gmail.com>
2026-06-11 13:43:30 -0600

    add(TestUtils) to remove duplicate code

---
+++ server/src/test/java/service/TestUtils.java
@@ -0,0 +1,27 @@
+package service;
+
+import dataaccess.DAOFactory;
+import exception.ResponseException;
+import org.junit.jupiter.api.Assertions;
+import response.AuthenticationResponse;
+
+public class TestUtils {
+    public static void assertAuthenticationResponse(AuthenticationResponse response, String username) {
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(response.username(), username);
+        Assertions.assertNotNull(response.authToken());
+    }
+
+    public interface Endpoint<T> {
+        void sendRequest(T request) throws ResponseException;
+    }
+
+    public static <T> void assertResponseException(T[] badRequests, Endpoint<T> endpoint, int errorCode, String errorMessage) {
+        for (T request : badRequests) {
+            ResponseException exception = Assertions.assertThrows(ResponseException.class, () ->
+                    endpoint.sendRequest(request));
+            Assertions.assertEquals(errorCode, exception.getErrorCode());
+            Assertions.assertTrue(exception.getMessage().contains(errorMessage));
+        }
+    }
+}
