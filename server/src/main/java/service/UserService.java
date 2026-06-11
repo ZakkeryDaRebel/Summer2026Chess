fb7b3c115d29f626ee9289455bb9e8d578f18168
Tristan Weech <twminebro@gmail.com>
2026-06-11 09:47:31 -0600

    add(UserService) class, variables, and constructor

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,15 @@
+package service;
+
+import dataaccess.AuthDAO;
+import dataaccess.UserDAO;
+
+public class UserService {
+
+    private final AuthDAO authDAO;
+    private final UserDAO userDAO;
+
+    public UserService(AuthDAO authDAO, UserDAO userDAO) {
+        this.authDAO = authDAO;
+        this.userDAO = userDAO;
+    }
+}
