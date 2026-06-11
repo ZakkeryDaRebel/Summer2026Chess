b0329c563993255ec8eeef17e2105d27e25c3d3e
Tristan Weech <twminebro@gmail.com>
2026-06-11 09:57:28 -0600

    add(UserService) method signatures for register, login, and logout

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -2,6 +2,9 @@ package service;
 
 import dataaccess.AuthDAO;
 import dataaccess.UserDAO;
+import request.LoginRequest;
+import request.RegisterRequest;
+import response.AuthenticationResponse;
 
 public class UserService {
 
@@ -11,5 +14,17 @@ public class UserService {
     public UserService(AuthDAO authDAO, UserDAO userDAO) {
         this.authDAO = authDAO;
         this.userDAO = userDAO;
+    }
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        return null;
+    }
+
+    public AuthenticationResponse login(LoginRequest request) {
+        return null;
+    }
+
+    public void logout(String authToken) {
+        return;
     }
 }
