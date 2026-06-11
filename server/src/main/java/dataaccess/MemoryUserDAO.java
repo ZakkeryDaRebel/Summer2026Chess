c9f458c57fed6e5e301b2b0f6a93ee8ece8d0509
Tristan Weech <twminebro@gmail.com>
2026-06-11 09:44:46 -0600

    add(MemoryUserDAO) create and get User methods

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -4,11 +4,23 @@ import model.UserData;
 
 import java.util.HashMap;
 
-public class MemoryUserDAO {
+public class MemoryUserDAO implements UserDAO {
 
     private HashMap<String, UserData> userDatabase;
 
     public MemoryUserDAO() {
         this.userDatabase = new HashMap<>();
+    }
+
+    public void createUser(UserData user) {
+        this.userDatabase.put(user.username(), user);
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        UserData user = this.userDatabase.get(username);
+        if (user == null) {
+            throw new DataAccessException("Unauthorized");
+        }
+        return user;
     }
 }
