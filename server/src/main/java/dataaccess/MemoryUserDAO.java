e75afe9f5061a2bb3f83ef49b65d4d7adf6e2848
Tristan Weech <twminebro@gmail.com>
2026-06-11 09:45:46 -0600

    add(MemoryUserDAO) clearUsers methods

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -23,4 +23,8 @@ public class MemoryUserDAO implements UserDAO {
         }
         return user;
     }
+
+    public void clearUsers() throws DataAccessException {
+        this.userDatabase = new HashMap<>();
+    }
 }
