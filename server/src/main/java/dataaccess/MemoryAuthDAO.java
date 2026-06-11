c98aa5d36f3ce2c4a3cfbd6518223372c868e375
Tristan Weech <twminebro@gmail.com>
2026-06-11 10:55:09 -0600

    add(MemoryAuthDAO) descriptive error message for getAuth

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -18,7 +18,7 @@ public class MemoryAuthDAO implements AuthDAO {
     public AuthData getAuth(String authToken) throws DataAccessException {
         AuthData auth = this.authDatabase.get(authToken);
         if (auth == null) {
-            throw new DataAccessException("Unauthorized");
+            throw new DataAccessException("Unauthorized: Please enter a valid username and password");
         }
         return auth;
     }
