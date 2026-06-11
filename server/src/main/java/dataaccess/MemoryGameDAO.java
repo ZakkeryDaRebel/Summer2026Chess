59621d2cd7212141f679f12822a1406685bc4b72
Tristan Weech <twminebro@gmail.com>
2026-06-11 09:04:57 -0600

    add(MemoryGameDAO) createGame method

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,14 +1,23 @@
 package dataaccess;
 
+import chess.ChessGame;
 import model.GameData;
-
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class MemoryGameDAO implements GameDAO {
 
-    private HashMap<Integer, GameData> gameDatabase;
+    private ArrayList<GameData> gameDatabase;
 
     public MemoryGameDAO() {
-        gameDatabase = new HashMap<>();
+        this.gameDatabase = new ArrayList<>();
+    }
+
+    public int createGame(String gameName) throws DataAccessException {
+        int gameID = this.gameDatabase.size();
+        GameData game = new GameData(gameID, null, null, gameName, new ChessGame());
+        if (!this.gameDatabase.add(game)) {
+            throw new DataAccessException("Faileed to create game");
+        }
+        return gameID;
     }
 }
