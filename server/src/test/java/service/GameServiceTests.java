a95af0b2c6d74f1024b2c29932a4b0b1eed7c7a0
Tristan Weech <twminebro@gmail.com>
2026-06-15 13:51:35 -0600

    add(GameServiceTests) listNoGames Successful

--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -1,12 +1,15 @@
 package service;
 
+import chess.ChessGame;
 import dataaccess.DAOFactory;
 import dataaccess.MemoryDAOFactory;
+import model.GameData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import request.CreateGameRequest;
 import response.CreateGameResponse;
+import response.ListGamesResponse;
 
 public class GameServiceTests {
 
@@ -43,5 +46,33 @@ public class GameServiceTests {
             TestUtils.assertUnauthorized(new CreateGameRequest[]{new CreateGameRequest(this.gameName)},
                     request -> this.gameService.createGame(request, token));
         }
+    }
+
+    public void assertList(ListGamesResponse response) {
+        Assertions.assertNotNull(response);
+        Assertions.assertNotNull(response.games());
+    }
+
+    @Test
+    public void listNoGamesSuccessful() {
+        String authToken = TestUtils.createAuthUser(this.userService);
+        ListGamesResponse response = this.gameService.listGames(authToken);
+        assertList(response);
+        Assertions.assertTrue(response.games().isEmpty());
+    }
+
+    @Test
+    public void listOneGameSuccessful() {
+        String authToken = TestUtils.createAuthUser(this.userService);
+        createGameSuccessful();
+        ListGamesResponse response = this.gameService.listGames(authToken);
+        assertList(response);
+        Assertions.assertEquals(1, response.games().size());
+        GameData gameOne = response.games().iterator().next();
+        Assertions.assertEquals(this.gameName, gameOne.gameName());
+        Assertions.assertNull(gameOne.whiteUsername());
+        Assertions.assertNull(gameOne.blackUsername());
+        Assertions.assertEquals(new ChessGame(), gameOne.game());
+        Assertions.assertEquals(0, gameOne.gameID());
     }
 }
